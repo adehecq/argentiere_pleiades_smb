89a5628ed0e5ea24fa3bda1fbe14e876e992037c
adehecq <3285905+adehecq@users.noreply.github.com>
2023-10-10 14:58:10 +0200

    Linting

--- code/test_kriging.py
+++ code/test_kriging.py
@@ -14,15 +14,15 @@ TODO:
 Author: Amaury Dehecq
 """
 
-import xdem
-import geoutils as gu
-import matplotlib.pyplot as plt
-import geopandas as gpd
-import numpy as np
-import gstools as gs
-import time
 import os
+import time
 
+import geopandas as gpd
+import geoutils as gu
+import gstools as gs
+import matplotlib.pyplot as plt
+import numpy as np
+import xdem
 
 # -- parameters -- #
 outdir = os.path.join("output", "thickness_kriging")
@@ -69,16 +69,16 @@ slope = xdem.terrain.slope(dem)
 # Plot input data
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
 
-arg_outline.show(fc='none', ec='k')
+arg_outline.show(fc="none", ec="k")
 sc = axes[0].scatter(zbed_x, zbed_y, c=H_obs)
 cb = plt.colorbar(sc)
 cb.set_label("Thickness (m)")
 
 velocity.show(ax=axes[1], cbar_title="Velocity (m/yr)", vmax=150)
-arg_outline.show(fc='none', ec='k', ax=axes[1])
+arg_outline.show(fc="none", ec="k", ax=axes[1])
 
 slope.show(ax=axes[2], cbar_title="Slope (degree)", vmax=60)
-arg_outline.show(fc='none', ec='k', ax=axes[2])
+arg_outline.show(fc="none", ec="k", ax=axes[2])
 
 plt.tight_layout()
 plt.show()
@@ -113,11 +113,23 @@ min_count = 3
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
 
 xdem.spatialstats.plot_1d_binning(
-    df, var_name="slope", statistic_name="mean", label_var="Slope (degrees)", label_statistic="Mean thickness (m)", ax=axes[0], min_count=min_count
+    df,
+    var_name="slope",
+    statistic_name="mean",
+    label_var="Slope (degrees)",
+    label_statistic="Mean thickness (m)",
+    ax=axes[0],
+    min_count=min_count,
 )
 
 xdem.spatialstats.plot_1d_binning(
-    df, var_name="velocity", statistic_name="mean", label_var="Velocity (m/yr)", label_statistic="Mean thickness (m)", ax=axes[1], min_count=min_count
+    df,
+    var_name="velocity",
+    statistic_name="mean",
+    label_var="Velocity (m/yr)",
+    label_statistic="Mean thickness (m)",
+    ax=axes[1],
+    min_count=min_count,
 )
 
 plt.tight_layout()
@@ -132,7 +144,7 @@ xdem.spatialstats.plot_2d_binning(
     label_var_name_1="Slope (degrees)",
     label_var_name_2="Velocity (m/yr)",
     label_statistic="Mean thickness (m)",
-    min_count=min_count
+    min_count=min_count,
 )
 plt.tight_layout()
 plt.show()
@@ -151,10 +163,10 @@ r = np.corrcoef(H_obs[np.isfinite(H_modeled_obs)], H_modeled_obs[np.isfinite(H_m
 # - Plot -
 
 # Plot one vs the other
-plt.plot(H_obs, H_modeled_obs, ls='', marker='+', c="C0")
+plt.plot(H_obs, H_modeled_obs, ls="", marker="+", c="C0")
 plt.xlabel("Observed thickness (m)")
 plt.ylabel("Modeled thickness (m)")
-plt.axline((0, 0), slope=1, color='k', ls='--')  # Plot 1:1 line
+plt.axline((0, 0), slope=1, color="k", ls="--")  # Plot 1:1 line
 plt.title(f"R2 = {r**2:.2f}")
 plt.show()
 
@@ -167,32 +179,40 @@ H_modeled = gu.Raster.from_array(H_modeled, transform=velocity.transform, crs=ve
 
 # Plot map along with map of residuals
 fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(18, 6))
-arg_outline.show(fc='none', ec='k', ax=axes[0])
+arg_outline.show(fc="none", ec="k", ax=axes[0])
 sc = axes[0].scatter(zbed_x, zbed_y, c=res)
 cb = plt.colorbar(sc)
 cb.set_label("Residual obs - model (m)")
 axes[1].hist(res, bins=40)
 axes[1].set_xlabel("Obs - model residual (m)")
 H_modeled.show(vmin=0, vmax=500, cbar_title="Modeled thickness (m)", ax=axes[2])
-arg_outline.show(fc='none', ec='k', ax=axes[2])
+arg_outline.show(fc="none", ec="k", ax=axes[2])
 plt.show()
 
 # - Calculate variogram on H residuals using xdem -
 
 # Sample each 50 m distance until 1 km, then every 200 m
 vg_bins = np.append(np.arange(0, 1500, 50), np.arange(1500, 5000, 200))
-#vg_bins = 30*np.array([np.sqrt(2)**i for i in range(1, 16)])  # xdem's default
+# vg_bins = 30*np.array([np.sqrt(2)**i for i in range(1, 16)])  # xdem's default
 
-vg_output = xdem.spatialstats.sample_empirical_variogram(res, coords=np.array((zbed_x, zbed_y)), subsample_method="cdist_point", bin_func = vg_bins)
+vg_output = xdem.spatialstats.sample_empirical_variogram(
+    res, coords=np.array((zbed_x, zbed_y)), subsample_method="cdist_point", bin_func=vg_bins
+)
 
-func_vgm, params_vgm = xdem.spatialstats.fit_sum_model_variogram(list_models=["Spherical"], empirical_variogram=vg_output)
+func_vgm, params_vgm = xdem.spatialstats.fit_sum_model_variogram(
+    list_models=["Spherical"], empirical_variogram=vg_output
+)
 print("\n## Variogram model with xdem")
 print(params_vgm)
 
 xdem.spatialstats.plot_variogram(
     vg_output,
-    list_fit_fun=[func_vgm,],
-    list_fit_fun_label=["Variogram model",],
+    list_fit_fun=[
+        func_vgm,
+    ],
+    list_fit_fun_label=[
+        "Variogram model",
+    ],
 )
 plt.tight_layout()
 plt.show()
@@ -245,7 +265,7 @@ print(f"Starting kriging at {time.strftime('%H:%M:%S', time.localtime())}")
 t0 = time.time()
 
 # downsampling factor
-dw=2
+dw = 2
 
 # with structured mesh, on the whole scene -> slower
 # x_coords = grid_x[0, :]
@@ -289,8 +309,8 @@ if dw > 1:
     krig_var_final = np.repeat(np.repeat(krig_var_final, dw, axis=0), dw, axis=1)
 
     # remove extra rows/columns that were added
-    krig_final = krig_final[:H_modeled.shape[0], :H_modeled.shape[1]]
-    krig_var_final = krig_var_final[:H_modeled.shape[0], :H_modeled.shape[1]]
+    krig_final = krig_final[: H_modeled.shape[0], : H_modeled.shape[1]]
+    krig_var_final = krig_var_final[: H_modeled.shape[0], : H_modeled.shape[1]]
 
 # Add back modeled H
 H_final = H_modeled + krig_final
